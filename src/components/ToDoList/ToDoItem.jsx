88fb54f12a7943621255312d83de3a236d55f5c2
Diana <dianavodopivec16@gmail.com>
2024-05-28 18:13:04 -0300

    style: adding new colors

--- src/components/ToDoList/ToDoItem.jsx
+++ src/components/ToDoList/ToDoItem.jsx
@@ -1,11 +1,16 @@
-const ToDoItem = ({content, id, isEditing, isDeleted}) => {
+const ToDoItem = ({content, id, date, deleteActions, isEditing,}) => {
     return(
         <section id={id} key={id}>
         <article>
             <p>{content}</p>
-            <div>
-                <button>Edit</button>
-                <button>Delete</button>
+            <div className="container-buttons">
+                <button>
+                    <img src="src/assets/edit.svg" alt="edit-svg" />
+                </button>
+                <button id={id} onClick={deleteActions}>
+                    <img className={"svg-delete"} src="src/assets/delete.svg" alt="delete-svg" />
+                </button>
+                <p>{date}</p>
             </div>
         </article>
         <div className="drop-down">{content}</div>
--- src/components/ToDoList/ToDoListComponent.jsx
+++ src/components/ToDoList/ToDoListComponent.jsx
@@ -1,36 +1,65 @@
-import { useState } from "react"
-import "/src/components/ToDoList/ToDoList.css"
+import { useEffect, useState } from "react";
+import "/src/components/ToDoList/ToDoList.css";
 import ToDoItem from "./ToDoItem";
 const regex = {
-    noWhiteSpace: /^(?!\s+$).+/,
+  noWhiteSpace: /^(?!\s+$).+/,
 };
 
-const ToDoListComponent = () => {
-    const [toDoList, setToDoList] = useState([])
+const ToDoContent = () => {
+  const [toDoList, setToDoList] = useState(
+    JSON.parse(localStorage.getItem("toDoListStorage")) || []
+  );
 
-    const addNewToDo = (e) => {
-        if(e.key === "Enter" && regex.noWhiteSpace.test(e.target.value)) {
-            const newToDo = {
-                idO: e.target.value + toDoList.length,
-                contentO: e.target.value,
-                isCompletedO: false,
-                dateO: new Date().getDate()
-            }
-            setToDoList((prevToDoList) => [...prevToDoList, newToDo])
-            e.target.value = ""
-        }
+  useEffect(() => {
+    localStorage.setItem("toDoListStorage", JSON.stringify(toDoList));
+  }, [toDoList]);
+
+  const addNewToDo = (e) => {
+    if (e.key === "Enter" && regex.noWhiteSpace.test(e.target.value)) {
+      const newToDo = {
+        idX: e.target.value + toDoList.length,
+        contentX: e.target.value,
+        isCompletedX: false,
+        dateX: new Date().getDate(),
+      };
+      setToDoList((prevState) => [...prevState, newToDo]);
+      e.target.value = "";
+      //console.log(newToDo)
     }
+  };
 
-    
+  const deleteToDo = (e) => {
+    const idToDelete = e.target.id;
+    const updatedToDoList = toDoList.filter((toDo) => toDo.idX !== idToDelete);
+    setToDoList(updatedToDoList);
+  };
 
-    return (
-        <>
-        <section className="primary-container">
-            <input type="text" placeholder="Create a new task!" onKeyUp={addNewToDo}/>
-            {toDoList.map(toDo => <ToDoItem id={toDo.idO} key={toDo.idO} content={toDo.contentO}/>)}
-        </section>
-        </>
-    )
-}
+  return (
+    <>
+      <section className="primary-container">
+        <input
+          type="text"
+          placeholder="Add New To-Do!"
+          onKeyDown={addNewToDo}
+        />
+        {toDoList.length > 0 ? (
+          toDoList.map((toDoList) => (
+            <ToDoItem
+              id={toDoList.idX}
+              key={toDoList.idX}
+              date={toDoList.dateX}
+              content={toDoList.contentX}
+              deleteActions={deleteToDo}
+            />
+          ))
+        ) : (
+          <p style={{ backgroundColor: "white", padding: "10px" }}>
+            Congratulations all completed!
+          </p>
+        )}
+      </section>
+    </>
+  );
+};
 
-export default ToDoListComponent
+export default ToDoContent;
